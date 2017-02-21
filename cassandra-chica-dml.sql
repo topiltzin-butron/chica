e8782661ba3e8bd103067cdc279daaae434046c5
topiltzin-butron <topiltzin-butron@users.noreply.github.com>
2017-02-21 14:08:49 +0700

    Created cassandra-chica-dml.sql

---
+++ cassandra-chica-dml.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_ADMIN', 'topi', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'topi@cafeconleche.xyz', 34);
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_DBA', 'topi', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'topi@cafeconleche.xyz', 34);
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_DBA', 'alex', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'alex@cafeconleche.xyz', 27);
+INSERT INTO user_roles (role, username, password, email, age) VALUES ('ROLE_ADMIN', 'hugo', '$2a$10$iNZser2g65z6gMT2C7OiZ.nQYpw2dyu.fAUasO..Ej8tpbUqddSq.', 'hugo@ehecatl.com', 35);
